8568ea913c5cb49bbdf41f7c80eae2ab700fbaac
Steve Johns <steve@stevejay.net>
2018-05-17 07:40:48 +0100

    fix windows build

--- data-service/build.js
+++ data-service/build.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
+if (!fs.existsSync('./build')){ fs.mkdirSync('./build'); }
 const obj = JSON.parse(fs.readFileSync('./content.json', 'utf8'));
 fs.writeFileSync('./build/content.txt', JSON.stringify(obj));
