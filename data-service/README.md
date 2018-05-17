6bd0c71223313358cce8809fb217e304d47115d3
Steve Johns <steve@stevejay.net>
2018-05-17 15:36:36 +0100

    add integration test

---
+++ data-service/README.md
@@ -0,0 +1,10 @@
+# Data Service
+
+## Prerequisites
+
+The file `./env.yml` must exist and be populated.
+
+## Development
+
+Run locally using `yarn run offline`. This will run the service against the
+development Elasticsearch instance specified in `./env.yml`.
---
+++ data-service/tests/integration/sitemap.test.js
@@ -0,0 +1,8 @@
+const request = require("request-promise-native");
+
+describe("sitemap.handler", () => {
+  it("should return a successful result", async () => {
+    const result = await request("http://localhost:3010/public/sitemap.txt");
+    expect(result).toEqual("");
+  });
+});
