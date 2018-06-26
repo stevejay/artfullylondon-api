10a68373741bea42ef0c62846471eebd6360dcbb
Steve Johns <steve@stevejay.net>
2018-06-26 11:49:47 +0100

    wip

--- tag-service/src/resolvers.js
+++ tag-service/src/resolvers.js
@@ -3,6 +3,7 @@ import * as tagType from "./tag-type";
 import * as validator from "./validator";
 import * as normaliser from "./normaliser";
 import * as mapper from "./mapper";
+import * as auth from "./lambda/auth";
 
 export default {
   Query: {
@@ -14,7 +15,11 @@ export default {
     })
   },
   Mutation: {
-    createTag: async (__, request) => {
+    createTag: async (__, request, c) => {
+      console.log("REQUEST", JSON.stringify(request), JSON.stringify(c));
+      // if (auth.isReadonlyUser(request)) {
+      //   throw new Error("[401] readonly user cannot modify system");
+      // }
       const tag = normaliser.normaliseCreateTagRequest(request.input.tag);
       validator.validateCreateTagRequest(tag);
       const dbTag = mapper.mapCreateTagRequest(tag);
--- tag-service/tests/integration/tag-graphql-mutation.test.js
+++ tag-service/tests/integration/tag-graphql-mutation.test.js
@@ -1,5 +1,4 @@
 import request from "request-promise-native";
-// import { sync } from "jest-toolkit";
 import { EDITOR_AUTH_TOKEN } from "../utils/cognito-auth";
 import { addToTable, truncateTagTable } from "../utils/dynamodb";
 jest.setTimeout(60000);
@@ -7,16 +6,6 @@ jest.setTimeout(60000);
 describe("tag graphql querying", () => {
   beforeAll(async () => {
     await truncateTagTable("artfullylondon-development-tag");
-    // await addToTable("artfullylondon-development-tag", {
-    //   tagType: "audience",
-    //   id: "audience/families",
-    //   label: "families"
-    // });
-    // await addToTable("artfullylondon-development-tag", {
-    //   tagType: "geo",
-    //   id: "geo/usa",
-    //   label: "usa"
-    // });
   });
 
   it("should support adding a tag", async () => {
